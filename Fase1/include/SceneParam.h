c9fc2b1d2b21689a10ea7275cd22d8609e091cbe
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-03-09 22:20:00 +0000

    Create SceneParam.h

---
+++ Fase1/include/SceneParam.h
@@ -0,0 +1,16 @@
+//
+// Created by Sandro Duarte on 09/03/2023.
+//
+
+#ifndef ENGINE_SCENEPARAM_H
+#define ENGINE_SCENEPARAM_H
+
+namespace cgengine {
+
+    class SceneParam {
+
+    };
+
+} // cgengine
+
+#endif //ENGINE_SCENEPARAM_H
