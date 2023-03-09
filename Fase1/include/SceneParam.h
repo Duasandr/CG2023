c84e6ab552faa47d0fc86914ced1268cf0f9355e
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-03-09 22:41:56 +0000

    Update SceneParam.h

--- Fase1/include/SceneParam.h
+++ Fase1/include/SceneParam.h
@@ -4,13 +4,37 @@
 
 #ifndef ENGINE_SCENEPARAM_H
 #define ENGINE_SCENEPARAM_H
+#include <string>
 
 namespace cg_engine {
 
     class SceneParam {
+    public:
+        SceneParam();
+        virtual ~SceneParam();
+
+        static SceneParam * Init(const char* pathToConfigXml);
+
+        void Destroy();
+
     private:
         float mWindowWidth;
         float mWindowHeight;
+        float mCameraPosX;
+        float mCameraPosY;
+        float mCameraPosZ;
+        float mCameraLookAtX;
+        float mCameraLookAtY;
+        float mCameraLookAtZ;
+        float mCameraUpX;
+        float mCameraUpY;
+        float mCameraUpZ;
+        float mCameraFOV;
+        float mCameraFar;
+        float mCameraNear;
+
+        unsigned int mModelCount;
+        std::string *mModelFileNames;
     };
 
 } // cg_engine
