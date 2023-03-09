6720a3cef0879b1cf97aea0d8352634a2be815f9
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-03-09 22:06:11 +0000

    Create engine.h

---
+++ Fase1/include/engine.h
@@ -0,0 +1,18 @@
+//
+// Created by Sandro Duarte on 09/03/2023.
+//
+
+#ifndef ENGINE_ENGINE_H
+#define ENGINE_ENGINE_H
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glew.h>
+#include <GL/glut.h>
+#endif
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+
+#endif //ENGINE_ENGINE_H
