135a7813acbe639f390e111d360bffc3c7afa7fd
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-03-09 22:29:25 +0000

    Create Vec3f.cpp

---
+++ Fase1/include/Vec3f.cpp
@@ -0,0 +1,40 @@
+//
+// Created by Sandro Duarte on 09/03/2023.
+//
+
+#include "Vec3f.h"
+
+namespace cg_engine_math {
+    Vec3f::Vec3f() {
+        mX =  mY = mZ = 0;
+    }
+
+    Vec3f::~Vec3f() {
+
+    }
+
+    void Vec3f::SetX(float x) {
+        mX = x;
+    }
+
+    void Vec3f::SetY(float y) {
+        mY = y;
+    }
+
+    void Vec3f::SetZ(float z) {
+        mZ = z;
+    }
+
+    float Vec3f::GetX() {
+        return mX;
+    }
+
+    float Vec3f::GetY() {
+        return mY;
+    }
+
+    float Vec3f::GetZ() {
+        return mZ;
+    }
+
+} // cg_engine_math
