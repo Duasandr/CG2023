6a17a4bd98d98a8613c6611f8ffa55fc5f2e31eb
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-03-10 16:54:32 +0000

    Create Box.h

---
+++ Fase1/include/generator/Box.h
@@ -0,0 +1,10 @@
+//
+// Created by Sandro Duarte on 10/03/2023.
+//
+
+#ifndef ENGINE_BOX_H
+#define ENGINE_BOX_H
+
+void Box(int length, int divisions, const char *filePath);
+
+#endif //ENGINE_BOX_H
