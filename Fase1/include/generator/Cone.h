9608fd514f9e7b18f1af82d6d49505688396d104
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-03-10 18:08:58 +0000

    Create Cone.h

---
+++ Fase1/include/generator/Cone.h
@@ -0,0 +1,10 @@
+//
+// Created by Sandro Duarte on 10/03/2023.
+//
+
+#ifndef ENGINE_CONE_H
+#define ENGINE_CONE_H
+
+void Cone(float radius, int height, int stacks, int slices, const char *fileName);
+
+#endif //ENGINE_CONE_H
