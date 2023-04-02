ce0bea1c89a3c13439fe12f5ca34cde0288cade9
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-04-02 13:17:41 +0100

    Update Camera.cpp
    Added documentation.

--- Fase2/src/engine/world/Camera.cpp
+++ Fase2/src/engine/world/Camera.cpp
@@ -8,6 +8,11 @@ namespace cg_engine {
     using tinyxml2::XMLElement;
     using cg_math::Vec3f;
 
+    /**
+     * @brief Default constructor.
+     * @details It initializes the position, the lookAt point, the up vector and the projection vector.
+     * It also initializes the radius, the alpha and the beta angles.
+     */
     Camera::Camera() {
         mPosition = Vec3f();
         mLookAt = Vec3f();
@@ -18,34 +23,38 @@ namespace cg_engine {
         mRadius = 0.0f;
     }
 
-    Camera::~Camera() {
-
-    }
-
     Camera *Camera::Create(tinyxml2::XMLElement *block) {
-        Camera *res = new Camera();
+        auto *res = new Camera();
 
+        // Parse the position of the camera from the XML block.
         XMLElement *tag = block->FirstChildElement("position");
 
+        // Set the position of the camera.
         res->SetPosition(Parser::ParseFloat(tag->Attribute("x")),
                                Parser::ParseFloat(tag->Attribute("y")),
                                Parser::ParseFloat(tag->Attribute("z")));
 
+        // Parse the lookAt point of the camera from the XML block.
         tag = block->FirstChildElement("lookAt");
 
+        // Set the lookAt point of the camera.
         res->SetLookAt(Parser::ParseFloat(tag->Attribute("x")),
                                Parser::ParseFloat(tag->Attribute("y")),
                                Parser::ParseFloat(tag->Attribute("z")));
 
+        // Parse the up vector of the camera from the XML block.
         tag = block->FirstChildElement("up");
 
+        // Set the up vector of the camera.
         res->SetUp(Parser::ParseFloat(tag->Attribute("x")),
                        Parser::ParseFloat(tag->Attribute("y")),
                        Parser::ParseFloat(tag->Attribute("z")));
 
 
+        // Parse the projection of the camera from the XML block.
         tag = block->FirstChildElement("projection");
 
+        // Set the projection of the camera.
         res->SetProjection(Parser::ParseFloat(tag->Attribute("fov")),
                                  Parser::ParseFloat(tag->Attribute("near")),
                                  Parser::ParseFloat(tag->Attribute("far")));
@@ -57,38 +66,27 @@ namespace cg_engine {
     void Camera::SetPosition(float x, float y, float z) {
         mPosition = Vec3f(x, y, z);
 
+        // The radius is the norm of the position vector.
         mRadius = mPosition.Norm();
+        // The alpha angle is the angle between the x-axis and the projection of the position vector on the xz plane.
         mAlpha = acos(y / mRadius);
+        // The beta angle is the angle between the z-axis and the projection of the position vector on the xz plane.
         mBeta = atan2(y, x);
     }
 
-    void Camera::SetLookAt(float x, float y, float z) {
-        mLookAt = Vec3f(x, y, z);
-    }
+    void Camera::SetLookAt(float x, float y, float z) { mLookAt = Vec3f(x, y, z); }
 
-    void Camera::SetUp(float x, float y, float z) {
-        mUp = Vec3f(x, y, z);
-    }
+    void Camera::SetUp(float x, float y, float z) { mUp = Vec3f(x, y, z); }
 
-    void Camera::SetProjection(float fov, float near, float far) {
-        mProjection = Vec3f(fov, near, far);
-    }
+    void Camera::SetProjection(float fov, float near, float far) { mProjection = Vec3f(fov, near, far); }
 
-    Vec3f Camera::GetPosition() const {
-        return mPosition;
-    }
+    Vec3f Camera::GetPosition() const { return mPosition; }
 
-    cg_math::Vec3f Camera::GetLookAt() const {
-        return mLookAt;
-    }
+    Vec3f Camera::GetLookAt() const { return mLookAt; }
 
-    cg_math::Vec3f Camera::GetUp() const {
-        return mUp;
-    }
-
-    cg_math::Vec3f Camera::GetProjection() const {
-        return mProjection;
-    }
+    Vec3f Camera::GetUp() const { return mUp; }
+    
+    Vec3f Camera::GetProjection() const { return mProjection; }
 
     void Camera::FreeMoveLeft() {
         mAlpha -= 0.1;
