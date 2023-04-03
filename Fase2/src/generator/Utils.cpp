372308db8e92effbe0c2c7969a1b2f4ff6ba4e4e
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-04-03 09:52:24 +0100

    Update Utils.cpp
    Added documentation.

--- Fase2/src/generator/Utils.cpp
+++ Fase2/src/generator/Utils.cpp
@@ -10,12 +10,14 @@ using std::ofstream;
 using std::vector;
 using cg_math::Vec3f;
 
-void DumpVertices(const char *path, const unsigned int totalValues, vector<Vec3f> &vertices) {
+void DumpVertices(const char *path, const unsigned int nVertices, vector<Vec3f> &vertices) {
     std::ofstream ofs;
 
     ofs.open(path);
 
-    ofs << totalValues << '\n';
+    // Write the number of vertices
+    ofs << nVertices << '\n';
+    // Write the vertices
     for (auto &vertex: vertices) {
         ofs << vertex << '\n';
     }
