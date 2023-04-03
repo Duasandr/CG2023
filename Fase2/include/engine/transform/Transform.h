25fec669ad4c066f7bef045b47b43ad66e0705cf
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-04-03 09:23:41 +0100

    Update Transform.h
    Added documentation.

--- Fase2/include/engine/transform/Transform.h
+++ Fase2/include/engine/transform/Transform.h
@@ -6,15 +6,38 @@
 #define ENGINE_TRANSFORM_H
 #include "tinyxml2.h"
 
+/**
+ * @brief cg_engine namespace contains all the classes and functions of the engine.
+ */
 namespace cg_engine {
 
+    /**
+     * @brief Transform class is the base class for all the transformations.
+     * @details It is an abstract class that contains the virtual method Apply. This method is used to apply the transformation.
+     */
     class Transform {
     public:
+        /**
+         * @brief Default constructor.
+         */
         Transform() = default;
+        /**
+         * @brief Default destructor.
+         */
         virtual ~Transform() =default;
 
+        /**
+         * @brief Create method is used to create a new transformation. Factory method.
+         * @details It is a static method that creates a new transformation based on the XML tag.
+         * @param tag XML tag that contains the information about the transformation.
+         * @return A pointer to the new transformation.
+         */
         static Transform *Create(tinyxml2::XMLElement *tag);
 
+        /**
+         * @brief Apply method is used to apply the transformation.
+         * @details It is a virtual method that is implemented in the derived classes.
+         */
         virtual void Apply() = 0;
     };
 
