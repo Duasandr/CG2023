c399bf20dee01a1db7ca6bbc8a74fa89109c1652
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-04-03 09:53:53 +0100

    Update Cone.h
    Added documentation.

--- Fase2/include/generator/Cone.h
+++ Fase2/include/generator/Cone.h
@@ -5,6 +5,15 @@
 #ifndef ENGINE_CONE_H
 #define ENGINE_CONE_H
 
+/**
+ * @brief Creates a cone.
+ * @details It creates a cone with the given radius, height, stacks and slices.
+ * @param radius
+ * @param height
+ * @param stacks
+ * @param slices
+ * @param fileName The path to the file where the cone will be saved.
+ */
 void Cone(float radius, int height, int stacks, int slices, const char *fileName);
 
 #endif //ENGINE_CONE_H
