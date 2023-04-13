50a9846c516ed792f5f350a42fb2e570818114bd
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-04-13 12:39:57 +0100

    Update
    Added documentation and refactoring on methods.

--- Fase2/include/common/Vec3f.h
+++ Fase2/include/common/Vec3f.h
@@ -11,27 +11,110 @@
 namespace cg_math {
 
     /**
-     * Represents a vector of three floats
+     * @brief Vec3f class.
+     * @details It represents a 3D vector.
      */
     class Vec3f {
     public:
+        /*
+         * //////////////////////////////
+         * // Friend functions
+         * //////////////////////////////
+         */
+
+        // We use friend functions to access private members of the class.
+
+        friend std::ostream& operator<<(std::ostream& os, const Vec3f& vec);
+
+        friend Vec3f operator+(const Vec3f& lhs, const Vec3f& rhs);
+        friend Vec3f operator-(const Vec3f& lhs, const Vec3f& rhs);
+        friend Vec3f operator*(const Vec3f& lhs, const Vec3f& rhs);
+
+        friend Vec3f operator+(const Vec3f& vec, float scalar);
+        friend Vec3f operator-(const Vec3f& vec, float scalar);
+        friend Vec3f operator*(const Vec3f& vec, float scalar);
+
+
+
+        /*
+         * //////////////////////////////
+         * // Constructors and destructor
+         * //////////////////////////////
+         */
+
         /**
          * @brief Default constructor.
-         * @details Sets x, y and z to 0.
+         * @details Sets x, y and z to 0. (0, 0, 0)
          */
         Vec3f();
         /**
-         * @brief Constructor.
-         * @details Sets x, y and z to the given values.
-         * @param x
-         * @param y
-         * @param z
+         * @brief Parameter constructor.
+         * @details Sets x, y and z to the given values. (x, y, z)
+         * @param x value of x
+         * @param y value of y
+         * @param z value of z
          */
         Vec3f(float x, float y, float z);
         /**
+         * @brief Copy constructor.
+         * @details Sets x, y and z to the values of the given vector.
+         * @param vec vector to copy
+         */
+        Vec3f(const Vec3f& vec);
+        /**
+         * @brief Move constructor.
+         * @details Move constructor is used to avoid unnecessary copies thus improving performance.
+         * It is defaulted because the compiler can generate a move constructor for us since it is a trivial class.
+         * @details Sets x, y and z to the values of the given vector.
+         * @param vec vector to move
+         */
+        Vec3f(Vec3f&& vec) noexcept = default;
+        /**
+         * @brief Parameter constructor.
+         * @details Sets x, y and z to the values of the given vector.
+         * Useful for converting from other types of vectors and achieving polymorphism.
+         * Explicit is used to avoid implicit conversions, this means that the compiler
+         * will not automatically convert from one type to another.
+         * E.g. Vec3f vec = {1, 2, 3} will not work but Vec3f vec({1, 2, 3}) will work.
+         * @param vec vector to copy
+         */
+        explicit Vec3f(std::vector<float> &vec);
+
+        /**
          * @brief Default destructor.
+         * @details Destructor is defaulted because the compiler can
+         * generate a destructor for us since it is a trivial class.
          */
         virtual ~Vec3f() = default;
+
+        /*
+         * //////////////////////////////
+         * // Static methods
+         * //////////////////////////////
+         */
+
+        /**
+         * Returns the norm (length) of the vector.
+         * @details The norm is the square root of the sum of the squares of the components of the vector.
+         * @param vec vector to get the norm
+         * @return float length of the vector
+        */
+        static float Norm       (const Vec3f& vec);
+        /**
+         * Returns a normalized vector.
+         * @details The normalized vector has the same direction as the original vector, but its length is 1.
+         * All components of the vector are divided by the norm of the vector.
+         * @param vec vector to normalize
+         * @return Vec3f normalized vector
+         */
+        static Vec3f Normalize  (const Vec3f& vec);
+        /**
+         * Returns the dot product of two vectors.
+         * @param lhs first vector
+         * @param rhs second vector
+         * @return float dot product
+         */
+        static Vec3f Cross      (const Vec3f& lhs, const Vec3f& rhs);
 
         /**
          * @brief Creates a cartesian vector from spherical coordinates.
@@ -50,6 +133,97 @@ namespace cg_math {
          */
         static Vec3f CreateCartesianFromPolar(float alpha, float height, float radius);
 
+        /*
+         * //////////////////////////////
+         * // Setters
+         * //////////////////////////////
+         */
+
+        /**
+         * @brief Sets the x component of the vector.
+         * @param x value to set
+         */
+        void SetX   (float x);
+        /**
+         * @brief Sets the y component of the vector.
+         * @param y value to set
+         */
+        void SetY   (float y);
+        /**
+         * @brief Sets the z component of the vector.
+         * @param z value to set
+         */
+        void SetZ   (float z);
+
+        /**
+         * @brief Sets the x, y and z components of the vector.
+         * @param x value to set
+         * @param y value to set
+         * @param z value to set
+         */
+        void Set    (float x, float y, float z);
+        /**
+         * @brief Sets the x, y and z components of the vector.
+         * @param vec vector to copy
+         */
+        void Set    (const Vec3f& vec);
+        /**
+         * @brief Sets the x, y and z components of the vector.
+         * @param vec vector to copy
+         */
+        void Set    (const std::vector<float> &vec);
+
+        /*
+         * //////////////////////////////
+         * // Getters
+         * //////////////////////////////
+         */
+
+        /**
+         * @brief Returns the x component of the vector.
+         * @return x value
+         */
+        float GetX() const;
+        /**
+         * @brief Returns the y component of the vector.
+         * @return y value
+         */
+        float GetY() const;
+        /**
+         * @brief Returns the z component of the vector.
+         * @return z value
+         */
+        float GetZ() const;
+
+
+        /*
+         * //////////////////////////////
+         * // Overloaded instance operators
+         * //////////////////////////////
+         */
+
+        /**
+         * @brief Overload of the -= operator.
+         * @details Subtracts the rhs vector.
+         * @param rhs vector to subtract
+         * @return reference to the this vector now with the subtraction applied
+         */
+        Vec3f &operator-=(const Vec3f& rhs);
+        /**
+         * @brief Overload of the += operator.
+         * @details Adds the rhs vector.
+         * @param rhs vector to add
+         * @return reference to the this vector now with the addition applied
+         */
+        Vec3f &operator+=(const Vec3f& rhs);
+
+    private:
+        /*
+         * //////////////////////////////
+         * // Private instance methods
+         * //////////////////////////////
+         */
+
         /**
          * @brief Converts spherical coordinates to cartesian coordinates.
          * @param alpha angle in the xy plane
@@ -65,86 +239,135 @@ namespace cg_math {
          */
         void PolarToCartesian(float alpha, float height, float radius);
 
-        /**
-         * Sets x value.
-         * @param x
+        /*
+         * //////////////////////////////
+         * // Private instance variables
+         * //////////////////////////////
          */
-        void SetX(float x);
 
-        /**
-         * Sets y value.
-         * @param y
-         */
-        void SetY(float y);
-
-        /**
-         * Sets z value.
-         * @param z
-         */
-        void SetZ(float z);
-
-        /**
-         * Gets x value.
-         * @return x
-         */
-        float GetX() const;
-
-        /**
-         * Gets y value.
-         * @return
-         */
-        float GetY() const;
-
-        /**
-         * Gets z value.
-         * @return
-         */
-        float GetZ() const;
-
-        /**
-         * Returns the norm (length) of the vector.
-         * @return
-        */
-        float Norm() const;
-
-        static Vec3f Normalize(const Vec3f& vec);
-
-        static Vec3f Cross(const Vec3f& vec1, const Vec3f& vec2);
-
-        Vec3f &operator-=(const Vec3f& vec2);
-        Vec3f &operator+=(const Vec3f& vec2);
-
-    private:
+        // x component of the vector
         float mX;
+        // y component of the vector
         float mY;
+        // z component of the vector
         float mZ;
     };
 
+    /*
+    * //////////////////////////////
+    * // Overloaded global operators
+    * //////////////////////////////
+    */
+
+    // We use the global operators to avoid having to overload the operators for every type of vector.
+    // For example, if we had a Vec3i, we would have to overload the operators for Vec3i and Vec3f.
+    // With the global operators, we only have to overload the operators for Vec3f and we can use them
+    // with any type of vector.
+    // The downside is that we have to pass the vectors as const references, which is not ideal.
+    // However, we can use the move constructor to avoid copying the vectors.
+    // For example, if we have a function that takes a Vec3f as a parameter, we can call it with
+    // a temporary vector, which will be moved to the function parameter.
+    // This way, we avoid copying the vector.
+    // For example:
+    // Vec3f vec(1, 2, 3);
+    // Vec3f vec2 = vec + Vec3f(1, 2, 3);
+    // In this case, the vector Vec3f(1, 2, 3) will be moved to the function parameter, avoiding a copy.
+    // If we had overloaded the operators for Vec3f and Vec3i, we would have to pass the vectors as
+    // const references, which would be less efficient.
+    // For example:
+    // Vec3f vec(1, 2, 3);
+    // Vec3i vec2(1, 2, 3);
+    // Vec3f vec3 = vec + vec2;
+    // In this case, we would have to pass the vectors as const references, which would be less efficient.
+    // However, we can still use the move constructor to avoid copying the vectors.
+    // For example:
+    // Vec3f vec(1, 2, 3);
+    // Vec3i vec2(1, 2, 3);
+    // Vec3f vec3 = vec + std::move(vec2);
+    // In this case, the vector vec2 will be moved to the function parameter, avoiding a copy.
+
+        /*
+         * //////////////////////////////
+         * // Stream operators
+         * //////////////////////////////
+        */
+
     /**
      * @brief Overload of the << operator.
-     * @details Prints the vector in the format x y z.
-     * @param os output stream to print to
-     * @param vec3F vector to print
-     * @return output stream with the vector printed
-     */
-    std::ostream &operator<<(std::ostream &os, const Vec3f& vec3F);
+     * @details Prints the vector to the given stream. Mainly used for debugging or logging.
+     * @param os stream to print to
+     * @param vec vector to print
+     * @return reference to the stream with the vector printed
+    */
+    std::ostream &operator<<(std::ostream &os, const Vec3f& vec);
+
+        /*
+        * //////////////////////////////
+        * // Arithmetic operators
+        * //////////////////////////////
+        */
+
+            /*
+            * //////////////////////////////
+            * // Arithmetic operators with vectors
+            * //////////////////////////////
+            */
 
     /**
      * @brief Overload of the + operator.
-     * @details Adds two vectors.
-     * @param vec1 first vector
-     * @param vec2 second vector
-     * @return sum of the two vectors
+     * @details Adds the two vectors. (x1 + x2, y1 + y2, z1 + z2)
+     * @param lhs left hand side vector
+     * @param rhs right hand side vector
+     * @return Vec3f vector with the addition applied. The original vectors are not modified.
      */
-    Vec3f operator+(const Vec3f& vec1, const Vec3f& vec2);
+    Vec3f operator+   (const Vec3f& lhs, const Vec3f& rhs);
+    /**
+     * @brief Overload of the - operator.
+     * @details Subtracts the two vectors. (x1 - x2, y1 - y2, z1 - z2)
+     * @param lhs left hand side vector
+     * @param rhs right hand side vector
+     * @return Vec3f vector with the subtraction applied. The original vectors are not modified.
+     */
+    Vec3f operator-   (const Vec3f& lhs, const Vec3f& rhs);
+    /**
+     * @brief Overload of the * operator.
+     * @details Multiplies the two vectors. (x1 * x2, y1 * y2, z1 * z2)
+     * @param lhs left hand side vector
+     * @param rhs right hand side vector
+     * @return Vec3f vector with the multiplication applied. The original vectors are not modified.
+     */
+    Vec3f operator*   (const Vec3f& lhs, const Vec3f& rhs);
 
-    Vec3f operator-(const Vec3f& vec1, const Vec3f& vec2);
+            /*
+            * //////////////////////////////
+            * // Arithmetic operators with scalars
+            * //////////////////////////////
+            */
 
-    Vec3f operator*(const Vec3f& vec1, const Vec3f& vec2);
-
-    Vec3f operator+(const Vec3f& vec1, float scalar);
-    Vec3f operator-(const Vec3f& vec1, float scalar);
-    Vec3f operator*(const Vec3f& vec1, float scalar);
+    /**
+     * @brief Overload of the + operator.
+     * @details Adds the scalar to the vector. (x + scalar, y + scalar, z + scalar)
+     * @param vec vector to add the scalar to
+     * @param scalar scalar to add
+     * @return Vec3f vector with the addition applied. The original vector is not modified.
+     */
+    Vec3f operator+   (const Vec3f& vec, float scalar);
+    /**
+     * @brief Overload of the - operator.
+     * @details Subtracts the scalar from the vector. (x - scalar, y - scalar, z - scalar)
+     * @param vec vector to subtract the scalar from
+     * @param scalar scalar to subtract
+     * @return Vec3f vector with the subtraction applied. The original vector is not modified.
+     */
+    Vec3f operator-   (const Vec3f& vec, float scalar);
+    /**
+     * @brief Overload of the * operator.
+     * @details Multiplies the vector by the scalar. (x * scalar, y * scalar, z * scalar)
+     * @param vec vector to multiply
+     * @param scalar scalar to multiply by
+     * @return Vec3f vector with the multiplication applied. The original vector is not modified.
+     */
+    Vec3f operator*   (const Vec3f& vec, float scalar);
 } // cg_math
 
 #endif //ENGINE_VEC3F_H
--- Fase2/src/common/cg_math/Vec3f.cpp
+++ Fase2/src/common/cg_math/Vec3f.cpp
@@ -2,129 +2,213 @@
 // Created by Sandro Duarte on 09/03/2023.
 //
 
+#include <vector>
 #include "Vec3f.h"
 
 /**
  * @brief cg_math namespace contains all the math classes and functions.
  */
 namespace cg_math {
+    using std::vector;
+    using std::pow;
+    using std::sqrt;
 
-    // Constructors
+    /*
+    * //////////////////////////////
+    * // Constructors
+    * //////////////////////////////
+    */
 
-    Vec3f::Vec3f() { mX = mY = mZ = 0; }
-
-    Vec3f::Vec3f(float x, float y, float z) {
-        mX = x;
-        mY = y;
-        mZ = z;
+    Vec3f::Vec3f()                          : mX(0), mY(0), mZ(0) {}
+    Vec3f::Vec3f(float x, float y, float z) : mX(x), mY(y), mZ(z) {}
+    Vec3f::Vec3f(const Vec3f &vec)          : Vec3f() { Set(vec); }
+    Vec3f::Vec3f(vector<float> &vec)        : Vec3f() {
+        if (vec.size() < 3) {
+            throw std::invalid_argument("Vector must have at least 3 elements.");
+        }
+        Set(vec);
     }
 
-    // Setters and getters
+    /*
+    * //////////////////////////////
+    * // Static methods
+    * //////////////////////////////
+    */
 
-    //Setters
+    Vec3f Vec3f::CreateCartesianFromPolar(float alpha, float height, float radius) {
+        float x = radius * cos(alpha);
+        float z = radius * sin(alpha);
 
-    void Vec3f::SetX(float x) { mX = x; }
-    void Vec3f::SetY(float y) { mY = y; }
-    void Vec3f::SetZ(float z) { mZ = z; }
+        return {x, height, z};
+    }
 
-    //Getters
+    Vec3f Vec3f::CreateCartesianFromSphere(float alpha, float beta, float radius) {
+        float x = radius * cos(beta) * sin(alpha);
+        float y = radius * sin(beta);
+        float z = radius * cos(beta) * cos(alpha);
+
+        return {x, y, z};
+    }
+
+    float Vec3f::Norm(const Vec3f &vec) {
+        // We use double precision to be consistent with the sqrt and pow functions
+        double delta = pow(vec.GetX(), 2) + pow(vec.GetY(), 2) + pow(vec.GetZ(), 2);
+
+        // Handle negative numbers due to floating point precision
+        if (delta < 0) { return 0; }
+
+        // We return from a static cast to float to be consistent with the rest of the code
+        // Also the loss of precision is not a problem
+        return static_cast<float>(sqrt(delta));
+    }
+
+    Vec3f Vec3f::Normalize(const Vec3f &vec) {
+        float norm = Norm(vec);
+
+        // Handle the case where the norm is 0 and avoid division by 0
+        // We return a vector with all 0s
+        if (norm == 0) { return {0,0,0}; }
+
+        return {vec.GetX() / norm, vec.GetY() / norm, vec.GetZ() / norm };
+    }
+
+    Vec3f Vec3f::Cross(const Vec3f &lhs, const Vec3f &rhs) {
+        float x = lhs.GetY() * rhs.GetZ() - lhs.GetZ() * rhs.GetY();
+        float y = lhs.GetZ() * rhs.GetX() - lhs.GetX() * rhs.GetZ();
+        float z = lhs.GetX() * rhs.GetY() - lhs.GetY() * rhs.GetX();
+
+        return {x, y, z};
+    }
+
+    /*
+    * //////////////////////////////
+    * // Setters
+    * //////////////////////////////
+    */
+
+    void Vec3f::SetX(float x)                   { mX = x; }
+    void Vec3f::SetY(float y)                   { mY = y; }
+    void Vec3f::SetZ(float z)                   { mZ = z; }
+    void Vec3f::Set (float x, float y, float z) {
+        // We use the setters for sake of consistency
+        SetX(x);
+        SetY(y);
+        SetZ(z);
+    }
+
+    void Vec3f::Set (const Vec3f &vec) {
+        Set(vec.GetX(), vec.GetY(), vec.GetZ());
+    }
+
+    void Vec3f::Set (const vector<float> &vec) {
+        Set(vec[0], vec[1], vec[2]);
+    }
+
+    /*
+    * //////////////////////////////
+    * // Getters
+    * //////////////////////////////
+    */
 
     float Vec3f::GetX() const { return mX; }
     float Vec3f::GetY() const { return mY; }
     float Vec3f::GetZ() const { return mZ; }
 
-    float Vec3f::Norm() const {
-        return sqrt(mX * mX + mY * mY + mZ * mZ);
-    }
-
-    // Static methods
-
-    Vec3f Vec3f::CreateCartesianFromPolar(float alpha, float height, float radius) {
-        Vec3f res;
-        res.PolarToCartesian(alpha, height, radius);
-        return res;
-    }
-
-    Vec3f Vec3f::CreateCartesianFromSphere(float alpha, float beta, float radius) {
-        Vec3f res;
-        res.SphereToCartesian(alpha, beta, radius);
-        return res;
-    }
-
-    // Non-static methods
+    /*
+    * //////////////////////////////
+    * // Instance methods
+    * //////////////////////////////
+    */
 
     void Vec3f::SphereToCartesian(float alpha, float beta, float radius) {
-        SetX(radius * cos(beta) * sin(alpha));
-        SetY(radius * sin(beta));
-        SetZ(radius * cos(beta) * cos(alpha));
+        float x = radius * cos(beta) * sin(alpha);
+        float y = radius * sin(beta);
+        float z = radius * cos(beta) * cos(alpha);
+
+        Set(x, y, z);
     }
 
     void Vec3f::PolarToCartesian(float alpha, float height, float radius) {
-        SetX(radius * cos(alpha));
-        SetY(height);
-        SetZ(radius * sin(alpha));
+        float x = radius * cos(alpha);
+        float z = radius * sin(alpha);
+
+        Set(x, height, z);
     }
 
-    Vec3f Vec3f::Normalize(const Vec3f &vec) {
-        float norm = vec.Norm();
-        if (norm == 0) {
-            return {0,0,0};
-        }
-        return {vec.GetX() / norm, vec.GetY() / norm, vec.GetZ() / norm };
-    }
 
-    // Overloaded operators
 
-    Vec3f &Vec3f::operator-=(const Vec3f &vec2) {
-        mX -= vec2.GetX();
-        mY -= vec2.GetY();
-        mZ -= vec2.GetZ();
+    /*
+    * //////////////////////////////
+    * // Overloaded operators
+    * //////////////////////////////
+    */
+
+    /*
+    * //////////////////////////////
+    * // Instance operators
+    * //////////////////////////////
+    */
+
+    Vec3f &Vec3f::operator-=(const Vec3f &rhs) {
+        Set(GetX() - rhs.GetX(), GetY() - rhs.GetY(), GetZ() - rhs.GetZ());
         return *this;
     }
 
-    Vec3f &Vec3f::operator+=(const Vec3f &vec2) {
-        mX += vec2.mX;
-        mY += vec2.mY;
-        mZ += vec2.mZ;
+    Vec3f &Vec3f::operator+=(const Vec3f &rhs) {
+        Set(GetX() + rhs.GetX(), GetY() + rhs.GetY(), GetZ() + rhs.GetZ());
         return *this;
     }
 
-    Vec3f Vec3f::Cross(const Vec3f &vec1, const Vec3f &vec2) {
-        return {vec1.GetY() * vec2.GetZ() - vec1.GetZ() * vec2.GetY(),
-                vec1.GetZ() * vec2.GetX() - vec1.GetX() * vec2.GetZ(),
-                vec1.GetX() * vec2.GetY() - vec1.GetY() * vec2.GetX()};
+    /*
+    * //////////////////////////////
+    * // Global Overloaded operators
+    * //////////////////////////////
+    */
+
+    std::ostream &operator<<(std::ostream &os, const Vec3f& vec) {
+        return  os << vec.GetX() << ' ' << vec.GetY() << ' ' << vec.GetZ();
     }
 
-    std::ostream &operator<<(std::ostream &os, const Vec3f& vec3F) {
-        return  os << vec3F.GetX() << ' ' << vec3F.GetY() << ' ' << vec3F.GetZ();
-    }
+    Vec3f operator+(const Vec3f& lhs, const Vec3f& rhs) {
+        float x = lhs.GetX() + rhs.GetX();
+        float y = lhs.GetY() + rhs.GetY();
+        float z = lhs.GetZ() + rhs.GetZ();
 
-    Vec3f operator+(const Vec3f& vec1, const Vec3f& vec2) {
-        return {vec1.GetX() + vec2.GetX(), vec1.GetY() + vec2.GetY(), vec1.GetZ() + vec2.GetZ()};
-    }
-
-
-    Vec3f operator+(const Vec3f& vec1, float scalar) {
-        return {vec1.GetX() + scalar, vec1.GetY() + scalar, vec1.GetZ() + scalar};
-    }
-
-    Vec3f operator-(const Vec3f& vec1, float scalar) {
-        return operator+(vec1, -scalar);
-    }
-
-    Vec3f operator-(const Vec3f& vec1, const Vec3f& vec2) {
-        return {vec1.GetX() - vec2.GetX(), vec1.GetY() - vec2.GetY(), vec1.GetZ() - vec2.GetZ()};
-    }
-
-    Vec3f operator*(const Vec3f& vec1, const Vec3f& vec2) {
-        return {vec1.GetX() * vec2.GetX(), vec1.GetY() * vec2.GetY(), vec1.GetZ() * vec2.GetZ()};
-    }
-
-    Vec3f operator*(const Vec3f& vec1, float scalar) {
-        return {vec1.GetX() * scalar, vec1.GetY() * scalar, vec1.GetZ() * scalar};
+        return {x, y, z};
     }
 
 
+    Vec3f operator+(const Vec3f& vec, float scalar) {
+        float x = vec.GetX() + scalar;
+        float y = vec.GetY() + scalar;
+        float z = vec.GetZ() + scalar;
 
+        return {x, y, z};
+    }
 
+    Vec3f operator-(const Vec3f& lhs, const Vec3f& rhs) {
+        // We use the + operator to avoid code duplication
+        return operator+(lhs, rhs * (-1));
+    }
+
+    Vec3f operator-(const Vec3f& vec, float scalar) {
+        // We use the + operator to avoid code duplication
+        return operator+(vec, -scalar);
+    }
+
+    Vec3f operator*(const Vec3f& lhs, const Vec3f& rhs) {
+        float x = lhs.GetX() * rhs.GetX();
+        float y = lhs.GetY() * rhs.GetY();
+        float z = lhs.GetZ() * rhs.GetZ();
+
+        return {x, y, z};
+    }
+
+    Vec3f operator*(const Vec3f& vec, float scalar) {
+        float x = vec.GetX() * scalar;
+        float y = vec.GetY() * scalar;
+        float z = vec.GetZ() * scalar;
+
+        return {x, y, z};
+    }
 } // cg_math
