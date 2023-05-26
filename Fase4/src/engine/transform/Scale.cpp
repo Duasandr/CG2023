8aaad1729f5fea1ccbd8c3bd585d604ead028daf
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-05-26 15:16:45 +0100

    Update Scale.cpp
    Added constructor with x, y and z attributes.
    Modified Apply with the same attributes

--- Fase4/src/engine/transform/Scale.cpp
+++ Fase4/src/engine/transform/Scale.cpp
@@ -12,10 +12,27 @@
 namespace cg_engine {
     using cg_math::Vec3f;
 
-    Scale::Scale() { mFactor = 1.0f; }
+    Scale::Scale() {
+        mFactor = 1.0f;
+        mScaleVec = nullptr;
+    }
 
-    Scale::Scale(float factor) { mFactor = factor; }
+    Scale::Scale(float factor) {
+        mFactor = factor;
+        mScaleVec = nullptr;
+    }
 
-    void Scale::Apply() { glScalef(mFactor, mFactor, mFactor); }
+    void Scale::Apply() {
+        if(mScaleVec != nullptr) {
+            glScalef(mScaleVec->GetX(), mScaleVec->GetY(), mScaleVec->GetZ());
+        }else{
+            glScalef(mFactor, mFactor, mFactor);
+        }
+    }
+
+    Scale::Scale(float x, float y, float z) {
+        mFactor = 1;
+        mScaleVec = new Vec3f(x,y,z);
+    }
 
 } // cg_engine
