b2c9d21c270a316ef9a6fe269cfd27f753687f1e
Sandro Duarte <57500696+Duasandr@users.noreply.github.com>
2023-06-03 18:03:08 +0100

    Update

--- Fase4/src/generator/primtives/Sphere.cpp
+++ Fase4/src/generator/primtives/Sphere.cpp
@@ -19,10 +19,12 @@ void Sphere(float radius, int stacks, int slices, const char *fileName) {
     // The angle between each stack
     float beta = static_cast<float>(M_PI) / static_cast<float>(stacks);
 
+    int halfStacks = static_cast<int>(std::ceil(stacks / 2));
+
     // Filling vertices of smaller circles each iteration
     for (int i = 0; i < slices; i++) {
         // stacks / 2 because we are going to draw the upper half and the lower half simultaneously
-        for (int j = 0; j < stacks / 2; j++) {
+        for (int j = 0; j < halfStacks; j++) {
 
             // Upper half
             vertices.push_back(Vec3f::CreateCartesianFromSphere(i * alpha, j * beta, radius));
@@ -58,27 +60,32 @@ void Sphere(float radius, int stacks, int slices, const char *fileName) {
     float delta = 1.0f / static_cast<float>(slices);
 
     for (int i = 0; i < slices; ++i) {
-        for (int j = 0; j < stacks/2; ++j) {
-            textureCoordinates.emplace_back(xTexture, yTexture, 0.0f);
-            textureCoordinates.emplace_back(xTexture + delta, yTexture, 0.0f);
-            textureCoordinates.emplace_back(xTexture, yTexture - delta, 0.0f);
+        for (int j = 0; j < halfStacks; ++j) {
+            float upperX = xTexture + delta;
+            float upperY = yTexture - delta;
 
-            textureCoordinates.emplace_back(xTexture + delta, yTexture, 0.0f);
-            textureCoordinates.emplace_back(xTexture + delta, yTexture - delta, 0.0f);
-            textureCoordinates.emplace_back(xTexture, yTexture - delta, 0.0f);
+            textureCoordinates.emplace_back(xTexture, 1 - yTexture, 0.0f);
+            textureCoordinates.emplace_back(upperX, 1 - yTexture, 0.0f);
+            textureCoordinates.emplace_back(xTexture, 1 - upperY, 0.0f);
 
-            textureCoordinates.emplace_back(xTexture, yTexture, 0.0f);
-            textureCoordinates.emplace_back(xTexture, yTexture - delta, 0.0f);
-            textureCoordinates.emplace_back(xTexture + delta, yTexture - delta, 0.0f);
+            textureCoordinates.emplace_back(upperX, 1 - yTexture, 0.0f);
+            textureCoordinates.emplace_back(upperX, 1 - upperY, 0.0f);
+            textureCoordinates.emplace_back(xTexture, 1 - upperY, 0.0f);
 
-            textureCoordinates.emplace_back(xTexture + delta, yTexture - delta, 0.0f);
-            textureCoordinates.emplace_back(xTexture + delta, yTexture, 0.0f);
-            textureCoordinates.emplace_back(xTexture, yTexture, 0.0f);
+            float lowerY = yTexture + delta;
+
+            textureCoordinates.emplace_back(xTexture, 1 - yTexture, 0.0f);
+            textureCoordinates.emplace_back(xTexture, 1 - lowerY, 0.0f);
+            textureCoordinates.emplace_back(upperX, 1 - lowerY, 0.0f);
+
+            textureCoordinates.emplace_back(upperX, 1 - lowerY, 0.0f);
+            textureCoordinates.emplace_back(upperX, 1 - yTexture, 0.0f);
+            textureCoordinates.emplace_back(xTexture, 1 -yTexture, 0.0f);
 
             yTexture -= delta;
         }
-        yTexture = 0.5f;
         xTexture += delta;
+        yTexture = 0.5f;
     }
 
 
